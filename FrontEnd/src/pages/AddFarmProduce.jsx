3a1964f701a4f46f9d845bf5af8989a12ebf7780
ObiomaIkpe <obiomaikpe1.com>
2024-02-08 12:05:40 +0100

    storing an array of objects

--- FrontEnd/src/pages/AddFarmProduce.jsx
+++ FrontEnd/src/pages/AddFarmProduce.jsx
@@ -1,47 +1,87 @@
 import React, { useState } from 'react'
-import { FaCirclePlus } from 'react-icons/fa6'
+import { FaCirclePlus } from 'react-icons/fa6';
+import { MdDelete } from "react-icons/md";
+
 
 const AddFarmProduce = () => {
-    const [farmProduce, setFarmProduce] = useState([])
+    const [farmProduce, setFarmProduce] = useState([]);
+    const [product, setProduct] = useState('');
+    const [quantity, setQuantity] = useState('');
 
-    const addProducts = () => {
+  
+    // start of array management functions  
+    const handleAddFarmProduce = () => {
+        const newProduct = {
+          product: product,
+          quantity: quantity
+        }
+        console.log(newProduct)
+        setFarmProduce(farmP => [...farmP, newProduct])
+        console.log(farmProduce)
         
-    } 
+    }
+    
+    const prods = farmProduce;
 
-  return (
-   
+    const handleRemoveFarmProduce = (index) => {
+    
+    }
+    //end of array management functions
 
-    <div className='p-3 max-w-lg mx-auto items-center justify-center '>
-      <h1 className='text-center text-sm'>Add Products</h1>
-    <form className='flex flex-col'>
-
-    <div className='relative'>
-      <input 
-      className='border-lime-700 border-2 py-2  w-full outline-none rounded-lg' 
-      placeholder='product type. Eg.Poultry Eggs, vegetables, yams...' 
-      type='' 
-      id='username'
-      required
-       />
-
-<div className='absolute top-3 right-3'><FaCirclePlus /> </div>
-    </div>
     
 
+  return ( 
 
-    <div>
-      <input 
-      className='border-lime-700 border-2 py-2 my-2 w-full outline-none rounded-lg' 
-      placeholder='quantiy produced' 
-      type='text' 
-      id='username'
-       />
-    </div>
+<>
+  <div className='max-w-lg mx-auto p-3 flex flex-col items-center justify-center'>
 
-      <button className='max-w-full bg-neutral-800 text-white'>submit</button>
+  <h2 className='mx-auto mb-4'>Add farm products</h2>
+      <form className='flex flex-col mt-8'>
 
-</form>
-    </div>
+
+        <div className='flex flex-col my-4'>
+      <label className="text-zinc-800 text-base font- font-['Inter']">Farm Product</label>
+            <input
+            type='text' 
+             onChange={(e) => setProduct( e.target.value)}
+            className='bg-neutral-100 justify-start items-center inline-flex outline-none' placeholder='Eg. Yams'
+            id="" />
+        </div>
+
+        <div className='flex flex-col my-4'>
+      <label className="text-zinc-800 text-base font-normal font-['Inter']">Quantity Produced</label>
+            <input
+            type='text'   
+            onChange={(e) => setQuantity( e.target.value)}
+            className='bg-neutral-100 justify-start items-center inline-flex outline-none' placeholder='Eg. 500 tubers'
+            id="" />
+        </div>
+
+        <button onClick={handleAddFarmProduce}
+        type='button'
+        className='w-full bg-lime-700 p-3 mt-8 rounded-lg'>
+          Add products
+        </button>
+
+        {
+          farmProduce.map((item, index) => (
+            <div key={index} className='flex flex-row items-center justify-center gap-4 w-full'>
+              <div>prod: {item.product}</div> <br /> <br />
+              <div>quantity: {item.quantity}</div>
+              <MdDelete size={40} onClick={(index) => handleRemoveFarmProduce()}/>
+            </div>
+          ))
+        }
+      
+      </form>
+
+
+
+
+  
+  </div>
+
+</>
 
     
 
